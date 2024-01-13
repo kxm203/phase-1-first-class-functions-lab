b9c9546474d1f7557ca77951fdf955911312b741
Kenneth J. McClelland Jr <kxm203@yahoo.com>
2024-01-12 19:41:33 -0500

    It is starting to all add up.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+const driversNames = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = function(driversArray) {
+    return driversArray.slice(0,2);
+};
+const returnLastTwoDrivers = function(driversArray) {
+    return driversArray.slice(-2);
+};
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+ 
+const createFareMultiplier = function(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    };
+};
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(driversArray, driversFunction) {
+    return driversFunction(driversArray);
+};
